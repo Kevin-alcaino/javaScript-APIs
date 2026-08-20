5766d4ed3ad70763f95fa13265f394076b39ec28
Kevin-alcaino <wthblck1@gmail.com>
2026-08-20 00:10:25 -0400

    se rellena el select con las claves del archivo .JSON

--- assets/javScript/srcipt.js
+++ assets/javScript/srcipt.js
@@ -6,12 +6,28 @@ const resultado = document.querySelector("#resultado")
 
 // 2. Función Asinc para cargar las monedas del archivo .JSON
 async function getMonedas() {
+    try{
     const res = await fetch("mindicador.json");
     const data = await res.json();
 
     // Se rellena el select con las claves del archivo JSON
     for (let moneda of data ){
         const item = data[clave];
+
+       
+            if (item && typeof item === "object" && item.codigo && item.valor) {
+                const option = document.createElement("option");
+                option.value = item.codigo;
+                option.textContent = item.nombre;
+
+                // Se guarda el valor númerico en el atributo "dataset"
+                option.dataset.valor = item.valor;
+                selectMoneda.appendChild(option);
+            }
     }
-    
+}  catch (error){
+    resultado.textContent = "Error al cargar monedas.";
+    console.error(error);
+} 
 }
+getMonedas();
